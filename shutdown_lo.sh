578ffd82a6bc125ba41beacf149c6ced8f4e0d27
Patrick Harvey <p1harvey@stanford.edu>
2014-11-30 21:13:29 -0800

    Added script to shut down lo interfaces and modified both to enable creation of a variable number of interfaces

--- shutdown_lo.sh
+++ shutdown_lo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# set up loopback interfaces
+# shutdown loopback interfaces
 # Must be run as su
 
 ARGC=0
@@ -13,8 +13,6 @@ if [ $# -lt 1 ]
 fi
 
 while [ $ARGC -gt 0 ]; do
-  ifconfig lo:${ARGC} 192.168.2.${ARGC}
-  # Prevent the interfaces from overlapping in the address space
-  ifconfig lo:${ARGC} netmask 255.255.255.255
+  ifconfig lo:${ARGC} down
   ARGC=$(expr $ARGC - 1)
 done
