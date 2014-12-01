988ab7ae797cd41857fd4a1791aadd99b4bea066
Patrick Harvey <p1harvey@stanford.edu>
2014-11-30 22:14:22 -0800

    Added .gitignore and unversioned generated cmake build files in libtins

--- libtins/build/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmclaren/project/cs244b_final_project/libtins/examples
-# Build directory: /home/dmclaren/project/cs244b_final_project/libtins/build/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- libtins/build/include/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmclaren/project/cs244b_final_project/libtins/include
-# Build directory: /home/dmclaren/project/cs244b_final_project/libtins/build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(tins)
